b2175f252b4e48fcefce3fc02d0b19744741b6b4
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-06-12 19:46:58 +0600

    Login Page Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // Whenever the frontend pings its own /api/auth, intercept it
-        source: '/api/auth/:path*',
-        // Route it silently behind the scenes to the backend serverless instance
-        destination: 'https://finance-tracker-server-eta.vercel.app/api/auth/:path*',
-      },
-      {
-        source: '/api/transactions/:path*',
-        destination: 'https://finance-tracker-server-eta.vercel.app/api/transactions/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
